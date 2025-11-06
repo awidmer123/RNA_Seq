f7b9fcb664f7f0be9624d701a6a05b6e40d96419
awidmer123 <awidmer123@users.noreply.github.com>
2025-11-06 16:11:27 +0100

    First Commit: Folder  and Directory Setup

---
+++ scripts/Script_QC.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#SBATCH --job-name=sample_1_lung
+#SBATCH --output=/data/users/awidmer/RNA_Seq/results/QC/fastqc_%j.out
+#SBATCH --error=/data/users/awidmer/RNA_Seq/logs/QC/fastqc_%j.err
+#SBATCH --time=02:10:00
+#SBATCH --mem=1G
+#SBATCH --cpus-per-task=1
+#SBATCH --partition=pibu_el8
+
+#Set paths
+CONTAINER="/container/apptainer/fastqc-0.12.1.sif"
+INPUT_DIR="/data/users/awidmer/RNA_Seq/data/raw_data/reads_Lung"
+OUTPUT_DIR="/data/users/awidmer/RNA_Seq/results/QC"
+
+#Run FastQC using Apptainer Container
+apptainer exec ${CONTAINER} fastqc \
+    ${INPUT_DIR}/*.fastq.gz \
+    -o ${OUTPUT_DIR} \
+    -t $SLURM_CPUS_PER_TASK
+
+echo "Task complete!"
